b3b7b277972e994add81ad8fe17cd418c724c0a1
Brevven <brevingame@gmail.com>
2022-01-13 22:09:26 -0800

    crucibles deadlock stacking, etc

--- carbon-recipe-final-stacking.lua
+++ carbon-recipe-final-stacking.lua
@@ -3,10 +3,11 @@
 local util = require("__bzcarbon__.data-util");
 
 if deadlock then
-  deadlock.add_stack("flake-graphite",  "__bzcarbon__/graphics/icons/stacked/flake-graphite-stacked.png", "deadlock-stacking-2", 64)
-  deadlock.add_stack("graphite",  "__bzcarbon__/graphics/icons/stacked/graphite-stacked.png", "deadlock-stacking-2", 128)
+  deadlock.add_stack("flake-graphite",  "__bzcarbon__/graphics/icons/stacked/flake-graphite-stacked.png", "deadlock-stacking-1", 64)
+  deadlock.add_stack("graphite",  "__bzcarbon__/graphics/icons/stacked/graphite-stacked.png", "deadlock-stacking-1", 128)
   deadlock.add_stack("diamond", "__bzcarbon__/graphics/icons/stacked/diamond-stacked.png" , "deadlock-stacking-2", 128)
   deadlock.add_stack("graphene", "__bzcarbon__/graphics/icons/stacked/graphene-stacked.png" , "deadlock-stacking-2", 128)
+  deadlock.add_stack("crucible", "__bzcarbon__/graphics/icons/stacked/crucible-stacked.png" , "deadlock-stacking-2", 128)
   if util.me.use_fullerenes() then
     deadlock.add_stack("fullerenes",  "__bzcarbon__/graphics/icons/stacked/fullerene-stacked.png", "deadlock-stacking-2", 128)
     deadlock.add_stack("nanotubes",  "__bzcarbon__/graphics/icons/stacked/nanotube-stacked.png", "deadlock-stacking-2", 128)
@@ -18,10 +19,11 @@ end
 
 -- Deadlock crating recipes
 if deadlock_crating then
-  deadlock_crating.add_crate("flake-graphite", "deadlock-crating-2")
-  deadlock_crating.add_crate("graphite", "deadlock-crating-2")
+  deadlock_crating.add_crate("flake-graphite", "deadlock-crating-1")
+  deadlock_crating.add_crate("graphite", "deadlock-crating-1")
   deadlock_crating.add_crate("diamond", "deadlock-crating-2")
   deadlock_crating.add_crate("graphene", "deadlock-crating-2")
+  deadlock_crating.add_crate("crucible", "deadlock-crating-2")
   if util.me.use_fullerenes() then
     deadlock_crating.add_crate("fullerenes", "deadlock-crating-2")
     deadlock_crating.add_crate("nanotubes", "deadlock-crating-2")
