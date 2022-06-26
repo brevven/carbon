2293df67e68ddbc2e44fc46e7bced45cc6e07f36
Brevven <brevingame@gmail.com>
2022-06-26 01:00:41 -0700

    248k

---
+++ compatibility/248k.lua
@@ -0,0 +1,4 @@
+local util = require("__bzcarbon__.data-util");
+
+util.remove_ingredient("fu_carbon_fiber_recipe", "fi_crushed_coal_item")
+util.add_ingredient("fu_carbon_fiber_recipe", "graphite", 3)
--- data-updates.lua
+++ data-updates.lua
@@ -2,6 +2,7 @@ require("carbon-recipe-updates")
 require("map-gen-preset-updates")
 require("carbon-matter")
 require("strange-matter")
+require("compatibility/248k")
 require("carbon-recipe-modules") -- Should be last import, need in both updates and final-fixes
 
 -- require("omni")
