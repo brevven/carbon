cc6097ee2f46302dc13023e75eb3af679030d5b4
Brevven <brevingame@gmail.com>
2022-08-01 13:09:42 -0700

    asteroid mining

--- data-updates.lua
+++ data-updates.lua
@@ -10,5 +10,15 @@ require("carbon-recipe-modules") -- Should be last import, need in both updates 
 
 local util = require("data-util");
 
+if mods.Asteroid_Mining then
+  require("__Asteroid_Mining__.scripts.addmodule")
+  if util.me.use_flake_graphite() then
+    amfunc.addtype("flake-graphite", {a = 0,r = 0.0,g = 0.2,b = 0.5})
+  end
+  if util.me.use_rough_diamond() then
+    amfunc.addtype("rough-diamond", {a = 0,r = 0.0,g = 0.2,b = 0.8})
+  end
+end
+
 -- Must be last
 util.create_list()
