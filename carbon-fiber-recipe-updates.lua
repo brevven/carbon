a1c2d394e51e3078ee698a4d5ad91658eb6ba186
Brevven <brevingame@gmail.com>
2022-09-24 20:48:37 -0700

    carbon fiber stuff

---
+++ carbon-fiber-recipe-updates.lua
@@ -0,0 +1,11 @@
+local util = require("data-util");
+
+if util.me.use_fiber() then
+  util.replace_ingredient("low-density-structure", "plastic-bar", "carbon-fiber")
+  util.replace_ingredient("combat-shotgun", "wood", "carbon-fiber")
+  
+  util.add_ingredient("exoskeleton-equipment", "carbon-fiber", 10)
+
+  util.replace_some_ingredient("flying-fortress", util.titanium_plate, 1000, "carbon-fiber", 2000)
+  util.replace_some_ingredient("flying-fortress", "steel-plate", 1000, "carbon-fiber", 2000)
+end
