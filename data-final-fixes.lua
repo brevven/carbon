8ad7b5bc20238ae79e2ec625104d78d973836a41
Brevven <brevingame@gmail.com>
2023-04-24 19:36:43 -0700

    minor noop for new utils

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -17,12 +17,14 @@ else
   amt_ec = util.get_amount("electronic-circuit")
   if amt_ec == 1 then
     util.multiply_recipe("electronic-circuit", 2)
+    util.set_recipe_time("electronic-circuit-stone", 0.5) -- faster but more complex
   end
   util.replace_some_ingredient("electronic-circuit", "copper-cable", 2, "graphite", 1)
   -- AAI alternate green circuit
   local amt_ec = util.get_amount("electronic-circuit-stone", "electronic-circuit")
   if amt_ec == 1 then
     util.multiply_recipe("electronic-circuit-stone", 2)
+    util.set_recipe_time("electronic-circuit-stone", 0.5) -- faster but more complex
   end
   util.replace_some_ingredient("electronic-circuit-stone", "copper-cable", 2, "graphite", 1)
 end
